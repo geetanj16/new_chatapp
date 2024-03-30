ac2c2b73ecfcf888f604ed2ec42f7ce4a50d3136
Geetanj Sharma <71605521+geetanj16@users.noreply.github.com>
2024-03-30 11:28:35 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '3306',
     }
 }
